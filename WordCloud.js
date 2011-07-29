f68ab00838de1d4cbc396b3b579b6ef545151bb0
Niels Laukens <niels.laukens@vrtmedialab.be>
2011-07-29 12:13:07 +0200

    Changed movement algorithm to use Potential field

--- WordCloud.js
+++ WordCloud.js
@@ -286,33 +286,53 @@ WordCloud.prototype.removedWord = function (word) {
 
 WordCloud.prototype.redraw = function() {
 	var startTime = +new Date();
+
+	// Create potentialField
+	pf = this.anchor.conePotentialField();
+	//var pf = this.anchor.conePotentialField();
+	for( var word in this.words) {
+		var wordObj = this.words[ word ];
+		if( ! wordObj.attached() ) continue;
+
+		var factor = 10;
+		var border = 5;
+		var l=-border, t=-border, r=wordObj.width()+border, b=wordObj.height()+border;
+		var d = 1;
+		while(t<=b && l<=r) {
+			// Increment the border at distance d
+			for(var x=l; x<=r; x++) {
+				pf.setEl( wordObj.x()+x, wordObj.y()+t,
+					pf.el( wordObj.x()+x, wordObj.y()+t ) + factor*d ); // top row
+				if( t != b ) // check that we don't run over the same row twice
+				pf.setEl( wordObj.x()+x, wordObj.y()+b,
+					pf.el( wordObj.x()+x, wordObj.y()+b ) + factor*d ); // bottom row
+			}
+			for(var y=t+1; y<b; y++) { // Exclude first & last row (already done above)
+				pf.setEl( wordObj.x()+l, wordObj.y()+y,
+					pf.el( wordObj.x()+l, wordObj.y()+y ) + factor*d ); // left column
+				if( l != r )
+				pf.setEl( wordObj.x()+r, wordObj.y()+y,
+					pf.el( wordObj.x()+r, wordObj.y()+y ) + factor*d ); // right column
+			}
+			l++; r--; t++; b--; // Shrink box by 1 pixel
+			d++; // increment distance
+		}
+	}
+
 	for( var word in this.words ) {
 		var wordObj = this.words[ word ];
 		if( ! wordObj.attached() ) continue;
-		var mvx = 0, mvy = 0;
-		{ // Go to center
-			var mv = [ this.anchor.width()/2
-			                      - (wordObj.x()+wordObj.width()/2),
-			           this.anchor.height()/2
-			                      - (wordObj.y()+wordObj.height()/2) ];
-			var length = Math.sqrt( Math.pow(mv[0],2) + Math.pow(mv[1],2) );
-			mv[0] /= length; mv[1] /= length;
-			mvx += .5*mv[0]; mvy += .2*mv[1]; // Asymetrical, to compensate for ellipsness
-		}
-		// repulse from others
-		for( var otherWord in this.words ) {
-			if( word == otherWord ) continue;
-			var otherWordObj = this.words[ otherWord ];
-			if( ! otherWordObj.attached() ) continue;
-			/* Check if the rectangles overlap */
-			var mv = rect_intersect(
-			    wordObj.x(), wordObj.y(), wordObj.width(), wordObj.height(),
-			    otherWordObj.x(), otherWordObj.y(), otherWordObj.width(), otherWordObj.height()
-			    );
-			if( mv != false ) { mvx += 10*mv[0]; mvy += 10*mv[1]; }
-		}
-		wordObj.moveRel( Math.round_away_from_zero(mvx),
-		                 Math.round_away_from_zero(mvy) );
+
+		// Sence potential {top,bottom}{left,right}
+		var ptl = pf.el( wordObj.x()                  , wordObj.y()                    );
+		var ptr = pf.el( wordObj.x() + wordObj.width(), wordObj.y()                    );
+		var pbl = pf.el( wordObj.x()                  , wordObj.y() + wordObj.height() );
+		var pbr = pf.el( wordObj.x() + wordObj.width(), wordObj.y() + wordObj.height() );
+
+		var mvx = ((ptl+pbl) - (ptr+pbr)) * .03;
+		var mvy = ((ptl+ptr) - (pbl+pbr)) * .03;
+
+		wordObj.moveRel( mvx, mvy );
 	}
 
 	{ // Feedback loops
